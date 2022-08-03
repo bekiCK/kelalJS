5b63b218daf3770df0f2c0696d64c9f57cef83cc
Beki <107795659+bekiCK@users.noreply.github.com>
2022-08-03 15:50:50 +0000

    just starting

---
+++ kelal.js
@@ -0,0 +1,20 @@
+function App(p) {
+  this.title = p.title
+  this.body = document.getElementById('App')
+  this.add = function(e) {
+    
+  }
+  return this;
+}
+function button(){
+  
+}
+ function text(){
+   
+ }
+function page(){
+  
+}
+function log(l){
+  console.log(l)
+}
